c542d7755340b1415f0b904284bfedbf35c5620a
alfianalamsyahjk <alfianalamsyah9a@gmail.com>
2023-05-21 15:07:56 +0700

    add more column to user entity

--- src/main/java/com/example/BooksStore/entity/User.java
+++ src/main/java/com/example/BooksStore/entity/User.java
@@ -18,5 +18,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String password;
+    private String role;
 
 }
