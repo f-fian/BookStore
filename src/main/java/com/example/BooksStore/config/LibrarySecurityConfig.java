87bf1e5e7e3b87402b5d2256dff739f649d10e9f
alfianalamsyahjk <alfianalamsyah9a@gmail.com>
2023-06-27 15:38:07 +0700

    rapihin code

--- src/main/java/com/example/BooksStore/config/LibrarySecurityConfig.java
+++ src/main/java/com/example/BooksStore/config/LibrarySecurityConfig.java
@@ -22,10 +22,12 @@ public class LibrarySecurityConfig {
     @Autowired
     private LogoutService logoutService;
     private String[] WHITE_LIST = {
-            "book/**",
-            "/auth/**","/user-book/**",
-            "register",
-            "authenticate","error","/logout"};
+            "/auth/**",
+            "/register",
+            "/authenticate","/error","/logout"};
+    private String[] ADMIN_URL = {"/book/**"};
+
+    private String[] AUTHENTICATED = {"/user-book/**","book/all",};
     @Autowired
     private CustomAuthenticationProvider customAuthenticationProvider;
 
@@ -41,9 +43,9 @@ public class LibrarySecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers("/book/add").hasAuthority("ADMIN")
                 .requestMatchers(WHITE_LIST).permitAll()
-                .anyRequest().permitAll()
+                .requestMatchers(AUTHENTICATED).authenticated()
+                .requestMatchers(ADMIN_URL).hasAuthority("ADMIN")
                 .and()
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic()
@@ -55,17 +57,12 @@ public class LibrarySecurityConfig {
                 .and()
                 .build();
     }
-
-
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
-
     @Autowired
     void registerProvider(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(customAuthenticationProvider);
     }
-
-
 }
